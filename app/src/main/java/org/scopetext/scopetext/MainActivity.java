399a89e9b6984b127819b0d962e23a81a9bf1ff4
Alton09 <altonqualls@gmail.com>
2016-01-11 16:52:47 -0700

    Added sliding page UI.

--- app/src/main/java/org/scopetext/scopetext/MainActivity.java
+++ app/src/main/java/org/scopetext/scopetext/MainActivity.java
@@ -1,31 +1,49 @@
 package org.scopetext.scopetext;
 
-import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    private final String NEW_CONTACT = "NEW_CONTACT";
+    final int PAGE_NUM = 2;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+    private Resources mResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_screen_slide);
+        mResources = getResources();
+
+        mPager = (ViewPager) findViewById(R.id.fragment_pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
 
         //TODO Put toolbar setup in private method
-        Toolbar toolbar = (Toolbar) findViewById(R.id.actionBar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.actionBar);
+      //  setSupportActionBar(toolbar);
+    }
 
-        // TODO Set color equal to a resource instead of hardcoded value
-        //toolbar.setTitleTextColor(0xFFFFFFFF);
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
     }
 
     @Override
@@ -38,18 +56,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_new_contact:
-                newContactFragment();
+            case R.id.action_custom_settings:
             default:
-                // Call super class if unrecognized button
                 return super.onOptionsItemSelected(item);
         }
     }
 
     private void newContactFragment() {
+        final String NEW_CONTACT = mResources.getString(R.string.new_contact);
         NewContactFragment fragment = NewContactFragment.newInstance();
+
         getSupportFragmentManager().beginTransaction().
                 add(R.id.fragment_container, fragment, NEW_CONTACT).
                 addToBackStack(NEW_CONTACT).commit();
+    }
+
+    /**
+     * A simple pager adapter that represents 2 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_NUM;
+        }
     }
 }
