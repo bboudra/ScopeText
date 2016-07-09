dbe20799e189c0ff9fc9f129f74485484d24c4de
John Qualls <altonqualls@gmail.com>
2016-07-09 00:40:15 -0600

    #6 Android contact search prototype: Following Google tutorial.

--- app/src/main/java/org/scopetext/scopetext/NewContactFragment.java
+++ app/src/main/java/org/scopetext/scopetext/NewContactFragment.java
@@ -7,25 +7,54 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.provider.ContactsContract.Contacts;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 
 /**
  * Handles the UI when the user adds a new contact.
  */
 public class NewContactFragment extends Fragment
-    implements LoaderCallbacks<Cursor>,
-               OnItemClickListener {
+    implements LoaderCallbacks<Cursor>, OnItemClickListener, OnEditorActionListener{
+
+    /**
+     * Replaces constructor.
+     * @return A new instance of fragment NewContactFragment.
+     */
+    // TODO: Pass in parameters for fragment communication.
+    public static NewContactFragment newInstance() {
+        NewContactFragment fragment = new NewContactFragment();
+        return fragment;
+    }
+
+    // Contains columns returned from query
+    @SuppressLint("InlinedApi")
+    private static final String[] PROJECTION =
+            {
+                    Contacts._ID,
+                    Contacts.LOOKUP_KEY,
+                    Build.VERSION.SDK_INT
+                            >= Build.VERSION_CODES.HONEYCOMB ?
+                            Contacts.DISPLAY_NAME_PRIMARY :
+                            Contacts.DISPLAY_NAME
+
+            };
+
     /*
  * Defines an array that contains column names to move from
  * the Cursor to the ListView.
@@ -45,6 +74,18 @@ public class NewContactFragment extends Fragment
     private final static int[] TO_IDS = {
             android.R.id.text1
     };
+
+    // Defines the text expression
+    @SuppressLint("InlinedApi")
+    private static final String SELECTION =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                    Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
+                    Contacts.DISPLAY_NAME + " LIKE ?";
+
+    // Defines a variable for the search string
+    private String mSearchString;
+    // Defines the array to hold values that replace the ?
+    private String[] mSelectionArgs = { mSearchString };
     // Define global mutable variables
     // Define a ListView object
     ListView mContactsList;
@@ -58,15 +99,11 @@ public class NewContactFragment extends Fragment
     // An adapter that binds the result Cursor to the ListView
     private SimpleCursorAdapter mCursorAdapter;
 
-    /**
-     * Replaces constructor.
-     * @return A new instance of fragment NewContactFragment.
-     */
-    // TODO: Pass in parameters for fragment communication.
-    public static NewContactFragment newInstance() {
-        NewContactFragment fragment = new NewContactFragment();
-        return fragment;
-    }
+    // The column index for the _ID column
+    private static final int CONTACT_ID_INDEX = 0;
+    // The column index for the LOOKUP_KEY column
+    private static final int LOOKUP_KEY_INDEX = 1;
+    private static final String LOG_TAG = "NewContactFragment.java";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,32 +116,84 @@ public class NewContactFragment extends Fragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        // Initializes the loader
+        getLoaderManager().initLoader(0, null, this);
+
         // Get ListView from parent activity
-        mContactsList = (ListView) getActivity().findViewById(R.id.contact_list);
+        mContactsList = (ListView) getActivity().findViewById(android.R.id.list);
 
         // Get a cursor adapter
         mCursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.new_contact_list_item,
                 null, FROM_COLUMNS, TO_IDS, 0);
         mContactsList.setAdapter(mCursorAdapter);
+
+        // Set on click listener to list view
+        mContactsList.setOnItemClickListener(this);
+
+        // Set edit text search button listener
+        EditText form = (EditText)getActivity().findViewById(R.id.search_edit_text);
+        form.setOnEditorActionListener(this);
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+    public void onItemClick(AdapterView<?> parent, View item, int position, long rowID) {
+        // Get the Cursor
+        SimpleCursorAdapter adapter = (SimpleCursorAdapter) parent.getAdapter();
+        Cursor cursor = adapter.getCursor();
+        // Move to the selected contact
+        cursor.moveToPosition(position);
+        // Get the _ID value
+        mContactId = cursor.getLong(CONTACT_ID_INDEX);
+        // Get the selected LOOKUP KEY
+        mContactKey = cursor.getString(LOOKUP_KEY_INDEX);
+        // Create the contact's content Uri
+        mContactUri = Contacts.getLookupUri(mContactId, mContactKey);
+        /*
+         * You can use mContactUri as the content URI for retrieving
+         * the details for a contact.
+         */
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
+        /*
+         * Makes search string into pattern and
+         * stores it in the selection array
+         */
+        mSelectionArgs[0] = "%" + mSearchString + "%";
+        // Starts the query
+        return new CursorLoader(
+                getActivity(),
+                Contacts.CONTENT_URI,
+                PROJECTION,
+                SELECTION,
+                mSelectionArgs,
+                null
+        );
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        // Delete the reference to the existing Cursor
+        mCursorAdapter.swapCursor(null);
+    }
 
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Put the result Cursor in the adapter for the ListView
+        mCursorAdapter.swapCursor(cursor);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+            Log.i(LOG_TAG, "Search Button clicked.");
+            // Get EditText input from the user
+            EditText form = (EditText) v;
+            String searchString = form.getText().toString();
+            Log.i(LOG_TAG, "Contact Search String: " + searchString);
+            mSearchString = searchString;
+            getLoaderManager().restartLoader(0, null, this);
+        }
+        return false;
     }
 }
